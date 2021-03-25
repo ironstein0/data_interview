2a3f06a704b8df2bfca10489c544c4325db46359
ironstein0 <riyansh.karani.011235@gmail.com>
2021-03-25 09:11:41 -0700

    normalizing features

--- src/train.py
+++ src/train.py
@@ -7,12 +7,13 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 from avro.datafile import DataFileReader
 from avro.io import DatumReader
+from sklearn import preprocessing
 
 
 DATA_FILE_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'takehome.avro')
 
-# read data from the avro file, and generate a pandas
-# dataframe out of it
+# read data from the avro file, and generate a train and
+# test features and labels out of it.
 def read_data():
     # read avro file into an array of dicts
     reader = DataFileReader(open(DATA_FILE_PATH, "rb"), DatumReader())
@@ -29,20 +30,40 @@ def read_data():
         # read the json into a pandas dataframe
         dataset =  pd.read_json(json_data)
 
-        # split into train and test data 
-        train_dataset = dataset.sample(frac=0.8, random_state=0)
-        test_dataset = dataset.drop(train_dataset.index)
-
         # separate features and labels
-        train_features = train_dataset.copy().drop('rating', 1).to_numpy()
-        test_features = test_dataset.copy().drop('rating', 1).to_numpy()
+        features = dataset.copy().drop('rating', 1)
+        labels = dataset.copy().pop('rating')
 
-        train_labels = left_shift_labels(train_dataset.copy().pop('rating'))
-        test_labels = left_shift_labels(train_dataset.copy().pop('rating'))
+        # normalize features
+        features = normalize_features(features)
+
+        # split into train and test data 
+        train_features = features.sample(frac=0.8, random_state=0)
+        test_features = features.drop(train_features.index)
+
+        train_labels = labels[labels.index.isin(train_features.index)]
+        test_labels = labels.drop(train_features.index)
+
+        # convert features to numpy arrays
+        train_features = train_features.to_numpy()
+        test_features = test_features.to_numpy()
+
+        # left shift labels to convert them from the range [1,10]
+        # to the range [0, 9]
+        train_labels = left_shift_labels(train_labels)
+        test_labels = left_shift_labels(test_labels)
 
         return train_features, train_labels, test_features, test_labels
     finally:
         reader.close()
+
+# normalizes all features to the range [0, 1]
+def normalize_features(dataset):
+    dataset = dataset.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    dataset_scaled = min_max_scaler.fit_transform(dataset)
+    dataset = pd.DataFrame(dataset_scaled)
+    return dataset
 
 # takes in a pandas series containing labels
 # from 1 to 10, and returns a numpy array containing
@@ -66,8 +87,7 @@ def train():
     # train
     model = tf.keras.Sequential([
         layers.InputLayer(input_shape=(num_features,)),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(128, activation='relu'),
+        layers.Dense(12, activation='relu'),
         layers.Dense(10)
     ])
     model.compile(
@@ -75,8 +95,8 @@ def train():
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
     )
-    model.fit(train_features, train_labels, epochs=100)
-    print(model.predict(train_features[:10]))
+    model.fit(train_features, train_labels, epochs=10)
+    print(model.predict(test_features[:10]))
 
 if __name__ == '__main__':
     train()
